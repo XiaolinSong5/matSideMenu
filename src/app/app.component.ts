d4433ebadc5ff8bf570d8d4df0e693c42de86ccb
Xiaolin <xiaolin.song@benerail.com>
2025-05-01 10:57:09 +0200

    material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {SidenavComponent} from './sidenav/sidenav.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+  CommonModule,
+  MatDialogModule,
+  MatIconModule,
+  RouterModule,
+  MatCardModule,
+MatSidenavModule,
+    SidenavComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'matSideMenu';
+  public isExpanded = false;
+
+  public toggleMenu() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {LocationsComponent} from './pages/locations.component';
+import {AboutComponent} from './pages/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+  path: "about",
+  component: AboutComponent,
+},
+  {
+    path: "locations",
+    component: LocationsComponent,
+  },
+
+  { path: "**", redirectTo: "about" },];
+
